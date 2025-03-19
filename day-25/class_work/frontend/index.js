76ce6ece9f4cbb663385474d7c7abacee278367e
subha123-ux <subhajitmal509@gmail.com>
2025-03-19 22:59:06 +0530

    Day 25

---
+++ day-25/class_work/frontend/index.js
@@ -0,0 +1,41 @@
+document.getElementById('registerForm')?.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const res = await fetch('http://localhost:8080/api/auth/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name, email, password })
+    });
+
+    console.log(res)
+
+    const data = await res.json();
+    console.log(data)
+    alert(data.message);
+    window.location.href="login.html"
+});
+
+document.getElementById('loginForm')?.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const res = await fetch('http://localhost:8080/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    });
+
+    const data = await res.json();
+    console.log(data)
+    if (data.token) {
+        localStorage.setItem('token', data.token);
+        window.location.href = 'dashboard.html';
+    } else {
+        alert('Login failed');
+    }
+});
+
